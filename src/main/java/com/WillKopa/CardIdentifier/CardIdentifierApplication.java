48a55b2d35552432a31abc69e7fd2b5b0da113c7
William Kopaczewski <will.kopaczewski@gmail.com>
2026-05-22 00:49:47 -0400

    Initial commit

---
+++ src/main/java/com/WillKopa/CardIdentifier/CardIdentifierApplication.java
@@ -0,0 +1,13 @@
+package com.WillKopa.CardIdentifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardIdentifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardIdentifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WillKopa/CardIdentifier/CardIdentifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WillKopa.CardIdentifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardIdentifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
